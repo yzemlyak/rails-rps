e9fb11e6232a0c1a546d3f1c3ff01b176575b488
yzemlyak <yzemlyak@gmail.com>
2025-04-22 04:58:39 +0000

    step

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,7 +1,14 @@
 class ZebraController < ApplicationController
   def giraffe 
     @random_move = ["rock, paper", "scissors"].sample
-    
+
+    if @random_move == "rock"
+      @result = "We tied!"
+    elsif @random_move == "scissors"
+      @result = "We won!"
+    else 
+      @result = "We lost!"
+    end
     
     render({ :template => "game_templates/play_rock" })
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get("/rock", { :controller => "zebra",:action => "giraffe" })
+  get("/paper", { :controller => "zebra",:action => "lion" })
+  get("/scissors", { :controller => "zebra",:action => "dolphin" })
+  get("/", { :controller => "zebra",:action => "home" })
 end
