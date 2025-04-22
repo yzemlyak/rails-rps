97bafd65851aa66c0cf79fa085a0156503451e27
yzemlyak <yzemlyak@gmail.com>
2025-04-22 05:05:08 +0000

    A draft at midnight

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -12,4 +12,32 @@ class ZebraController < ApplicationController
     
     render({ :template => "game_templates/play_rock" })
   end
+
+  def lion 
+    @random_move = ["rock, paper", "scissors"].sample
+
+    if @random_move == "paper"
+      @result = "We tied!"
+    elsif @random_move == "rock"
+      @result = "We won!"
+    else 
+      @result = "We lost!"
+    end
+    
+    render({ :template => "game_templates/play_rock" })
+  end
+
+  def dolphin 
+    @random_move = ["rock, paper", "scissors"].sample
+
+    if @random_move == "scissors"
+      @result = "We tied!"
+    elsif @random_move == "paper"
+      @result = "We won!"
+    else 
+      @result = "We lost!"
+    end
+    
+    render({ :template => "game_templates/play_rock" })
+  end
 end
